3e2a1123a3f41520b97f4124ee5dc003366c90a0
Michael Kelly <mibrkelly@gmail.com>
2022-07-17 16:06:22 -0500

    Testing

---
+++ jv/HelloWorld.java
@@ -0,0 +1,12 @@
+// https://www.partitionwizard.com/partitionmanager/javac-not-recognized-win10.html
+// set path="C:\Program Files\Java\jdk-18.0.1.1\bin"
+
+/*
+Trying to remember comments
+*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
