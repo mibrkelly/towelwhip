2d5307913dbef37bcb1639bb06ce38d4ac5fa6d2
Michael Kelly <mibrkelly@gmail.com>
2022-07-17 18:59:46 -0500

    More Updates

--- cherno/Game.java
+++ cherno/Game.java
@@ -50,11 +50,30 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	public void run() {
-		
-		
+		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
+		final double ns = 1000000000.0 / 60.0;
+		double delta = 0;
+		int frames = 0;
+		int updates = 0;
 		while (running) {
-			update();
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				update();
+				updates++;
+				delta--;
+			}
 			render();
+			frames++;
+
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				frame.setTitle("Rain | " + updates + " ups, " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
 		}
 	}
 
